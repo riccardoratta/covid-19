f88e2b88ef9bebbc5c9783a0eff176361a5edcd9
Riccardo Ratta <riccardoratta@gmail.com>
2020-10-29 12:41:00 +0100

    Remove semicolons

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,17 +1,17 @@
-import FileSync from 'lowdb/adapters/FileSync';
+import FileSync from 'lowdb/adapters/FileSync'
 import low from 'lowdb'
-import { Database } from './models';
+import { Database } from './models'
 
-const adapter = new FileSync<Database>('database.json');
+const adapter = new FileSync<Database>('database.json')
 
-export const d = low(adapter);
+export const d = low(adapter)
 
 
 export function parseMaybeInt(value: number | string) {
     if (typeof value === 'string') {
-        return parseInt(value);
+        return parseInt(value)
     }
 
-    return value;
+    return value
 }
 
